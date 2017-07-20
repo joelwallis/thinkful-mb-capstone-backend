ed5e6c0ffaed7a96e56a9cd71355bf054ed1f8c4
Joel Wallis Jucá <joelwallis@gmail.com>
2017-07-19 23:32:37 -0300

    Better interface for SunburstMapper

--- src/sunburst-mapper.js
+++ src/sunburst-mapper.js
@@ -5,11 +5,17 @@ const API_BASE_URL = 'https://sunburst.sunsetwx.com/v1'
 class SunburstMapper {
   constructor (params) {
     if (typeof params !== 'object') {
-      throw new Error('You must pass parameters as key/value pairs object.')
+      throw new Error([
+        'SunburstMapper requires an object with parameters.',
+        "\nE.g: `new SunburstMapper({",
+        "\n  email: 'john@doe.com',",
+        "\n  password: 'a1b2c3'",
+        "\n})`",
+      ].join(''))
     }
 
     if (!params.email || !params.password) {
-      throw new Error('`email` and `password` parameters are required.')
+      throw new Error('Parameters `email` and `password` are required.')
     }
 
     this.email = params.email;
@@ -19,24 +25,64 @@ class SunburstMapper {
   getToken () {
     return request.post({
       uri: API_BASE_URL + '/login',
-      formData: {
+      form: {
         email: this.email,
         password: this.password
       },
-      json: true
+      json: true,
+      simple: true
     })
-      .then(body => {
-        this.token = body.token
-        return this.token
+      .then(body => (this.token = body.token) && this.token)
+      .catch(error => {
+        throw new Error([
+          `Something went wrong while requesting the API with these credentials:`,
+          `Email: ${this.email}`,
+          `Password: ${this.password}`,
+          `Error: ${error}`
+        ].join("\n"))
       })
   }
 
-  getSunriseForecast () {
-    return this.getSunsetForecast()
-  }
+  predict (params) {
+    if (typeof params !== 'object')
+      throw new Error([
+        'SunburstMapper#predict requires an object with parameters.',
+        "\nE.g: `mySunburstMapperInstance.predict({",
+        "\n  type: 'sunrise',",
+        "\n  coords: '12.34567890,9.8765432',",
+        "\n})`",
+      ].join(''))
 
-  getSunsetForecast () {
-    return Promise.reject(new Error())
+    if (!params.type || !params.coords)
+      throw new Error('Parameters `type` and `coords` are required.')
+
+    if (!['sunrise', 'sunset'].includes(params.type))
+      throw new Error("Parameters `type` must be either 'sunrise' or 'sunset'.")
+
+    params.limit  = params.limit  || 1
+    params.radius = params.radius || 0
+
+    return (
+      this.token
+        ? Promise.resolve(this.token)
+        : this.getToken()
+    )
+      .then(token => request.get({
+        uri: API_BASE_URL + '/quality',
+        qs: params,
+        auth: {
+          bearer: this.token
+        },
+        json: true,
+        simple: true
+      }))
+      .then(geoJSON => ({
+        type: geoJSON.features[0].properties.type,
+        quality: geoJSON.features[0].properties.quality,
+        quality_percent: geoJSON.features[0].properties.quality_percent,
+        temperature: geoJSON.features[0].properties.temperature
+      }))
+      .catch(console.error)
   }
 }
 
--- test/sunburst-mapper.test.js
+++ test/sunburst-mapper.test.js
@@ -1,19 +1,35 @@
+import path from 'path'
+import request from 'request-promise-native'
 import SunburstMapper from '../src/sunburst-mapper'
 
 import chai from 'chai'
 import sinon from 'sinon'
-import request from 'request-promise-native'
 const expect = chai.expect
 
-const DEFAULT_EMAIL    = 'root@localhost.local'
-const DEFAULT_PASSWORD = '123'
-const DEFAULT_PARAMS   = {email: DEFAULT_EMAIL, password: DEFAULT_PASSWORD}
+import dotenv from 'dotenv'
+// dotenv.config({path: path.join(__dirname, '../.env')})
+
+// Immutable default parameters
+const DEFAULT_PARAMS = {}
+Object.defineProperties(DEFAULT_PARAMS, {
+  email: {value: 'root@localhost.local', writable: false},
+  password: {value: '123', writable: false}
+})
+
+// Immutable valid parameters (from environment variables - see dotenv module)
+const VALID_PARAMS = {}
+if (process.env.SUNBURST_ACCOUNT_EMAIL && process.env.SUNBURST_ACCOUNT_PASSWORD) {
+  Object.defineProperties(VALID_PARAMS, {
+    email: {value: process.env.SUNBURST_ACCOUNT_EMAIL, writable: false},
+    password: {value: process.env.SUNBURST_ACCOUNT_PASSWORD, writable: false}
+  })
+}
 
 describe('SunburstMapper', function () {
 
   it('is a class', function () {
     expect(typeof SunburstMapper).to.equal('function')
-    expect(() => new SunburstMapper()).to.throw()
+    expect(() => new SunburstMapper()).to.throw(Error)
     expect(function () {
       var sm = new SunburstMapper(DEFAULT_PARAMS)
       expect(sm).to.be.an.instanceof(SunburstMapper)
@@ -21,42 +37,154 @@ describe('SunburstMapper', function () {
   })
 
   it('requires a parameter `email`', function () {
-    expect(() => new SunburstMapper({password: DEFAULT_PASSWORD})).to.throw()
+    expect(() => new SunburstMapper({password: DEFAULT_PARAMS.password})).to.throw(Error)
     expect(() => new SunburstMapper(DEFAULT_PARAMS)).to.not.throw()
   })
 
   it('requires a parameter `password`', function () {
-    expect(() => new SunburstMapper({email: DEFAULT_EMAIL})).to.throw()
+    expect(() => new SunburstMapper({email: DEFAULT_PARAMS.email})).to.throw(Error)
     expect(() => new SunburstMapper(DEFAULT_PARAMS)).to.not.throw()
   })
 
   describe('#getToken', function () {
 
-    beforeEach(() => sinon.stub(request, 'post').returns(Promise.resolve({
-      token: 'abc'
-    })))
-    afterEach(() => request.post.restore && request.post.restore())
+    beforeEach(() => {
+      request.post.restore && request.post.restore()
+
+      sinon.stub(request, 'post').returns(Promise.resolve({
+        token: 'abc'
+      }))
+    })
+    afterEach(() => request.post.restore())
 
     it('gets an auth token (JWT) from Sunburst API', function () {
-      const sm = new SunburstMapper(DEFAULT_PARAMS)
-      sm.getToken()
-      request.post.called
-      const promise = sm.getToken()
-      return promise
+      return new SunburstMapper(DEFAULT_PARAMS)
+        .getToken()
         .then(token => expect(token).to.be.a('string'))
-        .catch(console.error)
     })
 
-    it('throws an error when something goes wrong', function () {
-      request.post.restore()
-      sinon.stub(request, 'post').returns(Promise.reject(new Error()))
-      const spy = sinon.spy()
-      const sm = new SunburstMapper(DEFAULT_PARAMS)
-      const promise = sm.getToken()
-      return promise
-        .catch(spy)
-        .then(() => spy.called)
+  })
+
+  describe('#predict', function () {
+
+    beforeEach(() => {
+      sinon.stub(request, 'post').returns(Promise.resolve({token: 'abc'}))
+      sinon.stub(request, 'get').returns(Promise.resolve({
+        features: [{
+            properties: {
+              type: 'Sunrise',
+              quality: 'Poor',
+              quality_percent: 23.38,
+              quality_value: -235.076,
+              temperature: 5.94
+            }
+        }]
+      }))
     })
+    afterEach(() => {
+      request.post.restore()
+      request.get.restore()
+    })
+
+    it('requires an object with parameters', function () {
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict()).to.throw(Error)
+    })
+
+    it('requires a parameter `type`', function () {
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict({
+        coords: '12.34,56.78'
+      })).to.throw(Error)
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict({
+        type: 'sunrise',
+        coords: '12.34,56.78'
+      })).to.not.throw()
+    })
+
+    it('requires a parameter `coords`', function () {
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict({
+        type: 'sunrise'
+      })).to.throw(Error)
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict({
+        type: 'sunrise',
+        coords: '12.34,56.78'
+      })).to.not.throw()
+    })
+
+    it("requires either 'sunrise' or 'sunset' as parameter `type`", function () {
+      expect(() => new SunburstMapper(DEFAULT_PARAMS).predict({
+        type: 'something else',
+        coords: '12.34,56.78'
+      })).to.throw(Error)
+      expect(() => [
+        'sunrise',
+        'sunset'
+      ].forEach(type => new SunburstMapper(DEFAULT_PARAMS).predict({
+        type: type,
+        coords: '12.34,56.78'
+      }))).to.not.throw()
+    })
+
+  })
+
+  describe('and the Sunburst API', function () {
+
+    // Tests are run if - and only if there are valid credentials set as
+    // env variables (`SUNBURST_ACCOUNT_EMAIL` and `SUNBURST_ACCOUNT_PASSWORD`)
+    (function (it) {
+
+      let sm
+
+      it('gets an auth token (JWT) from Sunburst API', function () {
+        this.timeout(5000)
+
+        sm = new SunburstMapper(VALID_PARAMS)
+        sm.getToken()
+          .then(token => expect(token).to.be.a('string'))
+
+        return sm
+      })
+
+      it('predicts the sunrise quality', function () {
+        this.timeout(5000)
+
+        return sm.predict({
+          type: 'sunrise',
+          coords: '-77.8600012,40.7933949'
+        })
+          .then(prediction => {
+            expect(prediction).to.be.an('object')
+            expect(prediction).to.have.property('type')
+            expect(prediction).to.have.property('quality')
+            expect(prediction).to.have.property('quality_percent')
+            expect(prediction).to.have.property('temperature')
+            expect(prediction.type).to.be.a('string')
+            expect(prediction.quality).to.be.a('string')
+            expect(prediction.quality_percent).to.be.a('number')
+            expect(prediction.temperature).to.be.a('number')
+          })
+      })
+
+      it('predicts the sunset quality', function () {
+        this.timeout(5000)
+
+        return sm.predict({
+          type: 'sunset',
+          coords: '-77.8600012,40.7933949'
+        })
+          .then(prediction => {
+            expect(prediction).to.be.an('object')
+            expect(prediction).to.have.property('type')
+            expect(prediction).to.have.property('quality')
+            expect(prediction).to.have.property('quality_percent')
+            expect(prediction).to.have.property('temperature')
+            expect(prediction.type).to.be.a('string')
+            expect(prediction.quality).to.be.a('string')
+            expect(prediction.quality_percent).to.be.a('number')
+            expect(prediction.temperature).to.be.a('number')
+          })
+      })
+
+    })(process.env.SUNBURST_ACCOUNT_EMAIL && process.env.SUNBURST_ACCOUNT_PASSWORD ? it : xit)
 
   })
 
