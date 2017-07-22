f3a0c9ffc31210d55e0b36b9d6a60ef8e530470b
Joel Wallis Jucá <joelwallis@gmail.com>
2017-07-22 01:29:01 -0300

    Better naming for the main endpoint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from 'express'
+import prediction from './prediction-router'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
-import sunrise from './sunrise-router'
-
 const port = process.env.PORT || 8080
 const app  = express()
 
-app.use('/sunrise', sunrise)
-
 app.get('/', (req, res) => res.end('Hello, World!'))
+
+app.use('/predict', prediction)
 
 app.listen(port, _ => console.log('Listening on', port))
